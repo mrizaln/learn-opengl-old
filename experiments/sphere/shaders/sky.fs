85f7f6fe62a24847d2dd738d351fb2a8ba34571c
mrizaln <mrizaln2000@gmail.com>
2022-07-06 23:43:03 +0700

    add a normPow() function that increase the brightness of sky texture in sky.fs shader file

--- experiments/sphere/shaders/sky.fs
+++ experiments/sphere/shaders/sky.fs
@@ -7,11 +7,13 @@ out vec4 FragColor;
 uniform sampler2D texture0;
 
 vec4 sigmoid(vec4, float);
+vec4 normPow(vec4, int);
 
 void main()
 {
     vec4 textureFrag = texture(texture0, TexCoord);
-    textureFrag = sigmoid(textureFrag, 10.0);
+    // textureFrag = sigmoid(textureFrag, 10.0);
+    textureFrag = normPow(textureFrag, 2);
     FragColor = textureFrag;
 }
 
@@ -21,6 +23,16 @@ vec4 sigmoid(vec4 avec4, float a)
     avec4.x = 1/(exp(-a*(avec4.x-0.5))+1);
     avec4.y = 1/(exp(-a*(avec4.y-0.5))+1);
     avec4.z = 1/(exp(-a*(avec4.z-0.5))+1);
+
+    return avec4;
+}
+
+// return 1 - pow(-1, factor)*pow(avec4 - 1, factor)
+vec4 normPow(vec4 avec4, int factor)
+{
+    avec4.x = 1.0 - pow(-1.0, factor)*pow(avec4.x - 1.0, factor);
+    avec4.y = 1.0 - pow(-1.0, factor)*pow(avec4.y - 1.0, factor);
+    avec4.z = 1.0 - pow(-1.0, factor)*pow(avec4.z - 1.0, factor);
 
     return avec4;
 }
